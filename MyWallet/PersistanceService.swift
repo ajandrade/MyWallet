cfd2da0647874214d2c99752b16f14c982595ae1
Amadeu Andrade <amadeu.dev@gmail.com>
2017-09-11 20:47:32 +0100

    Added persistance service

---
+++ MyWallet/PersistanceService.swift
@@ -0,0 +1,61 @@
+//
+//  PersistanceService.swift
+//  MyWallet
+//
+//  Created by Amadeu Andrade on 11/09/2017.
+//  Copyright © 2017 Amadeu Andrade. All rights reserved.
+//
+
+import Foundation
+
+protocol PersistanceServiceRepresentable {
+  associatedtype T
+  func save(_ items: [T])
+  func fetchAccounts() -> [T]
+}
+
+class PersistanceService {
+  
+  // MARK: - PRIVATE PROPERTIES
+  
+  fileprivate let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+  
+  fileprivate var accountsFileURL: URL {
+    return documentsDirectoryURL
+      .appendingPathComponent("Accounts")
+      .appendingPathExtension("plist")
+  }
+  
+  // MARK: - HELPERS
+  
+  fileprivate func createFakeData() -> [Account] {
+    let incomeTransaction = Transaction(amount: 1234.26, description: "Some work", date: Date(), category: .income)
+    let transportTransaction = Transaction(amount: 50, description: "Bus", date: Date(), category: .transport)
+    let funTransaction = Transaction(amount: 21.44, description: "Bar", date: Date(), category: .fun)
+    let accounts = [
+      Account(name: "Account Name", bank: "Bank Name", number: "01234567890123456789", transactions: [incomeTransaction, transportTransaction, funTransaction]),
+      Account(name: "Account Name", bank: "Bank Name", number: "01234567890123456789", transactions: [incomeTransaction, transportTransaction, funTransaction]),
+      Account(name: "Account Name", bank: "Bank Name", number: "01234567890123456789", transactions: [incomeTransaction, transportTransaction, funTransaction]),
+      Account(name: "Account Name", bank: "Bank Name", number: "01234567890123456789", transactions: [incomeTransaction, transportTransaction, funTransaction]),
+      Account(name: "Account Name", bank: "Bank Name", number: "01234567890123456789", transactions: [incomeTransaction, transportTransaction, funTransaction])
+    ]
+    save(accounts)
+    return accounts
+  }
+  
+}
+
+extension PersistanceService: PersistanceServiceRepresentable {
+  
+  func save(_ accounts: [Account]) {
+    let accountsPlist = accounts.map { $0.plistRepresentation } as NSArray
+    accountsPlist.write(to: accountsFileURL, atomically: true)
+  }
+  
+  func fetchAccounts() -> [Account] {
+    guard let accountsPlist = NSArray(contentsOf: accountsFileURL) as? [PlistData] else {
+      return createFakeData()
+    }
+    return accountsPlist.map(Account.init(plist:))
+  }
+}
---
+++ MyWallet/PlistRepresentable.swift
@@ -0,0 +1,16 @@
+//
+//  PlistRepresentable.swift
+//  MyWallet
+//
+//  Created by Amadeu Andrade on 11/09/2017.
+//  Copyright © 2017 Amadeu Andrade. All rights reserved.
+//
+
+import Foundation
+
+typealias PlistData = [String: Any]
+
+protocol PlistRepresentable {
+  var plistRepresentation: PlistData { get }
+  init(plist: PlistData)
+}
