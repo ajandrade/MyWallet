acc571ac7400a571b76b2e38faa4a0590726133a
Amadeu Andrade <amadeu.dev@gmail.com>
2017-09-11 20:49:38 +0100

    Created AccountsListViewController

---
+++ MyWallet/AccountsListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AccountsListViewController.swift
+//  MyWallet
+//
+//  Created by Amadeu Andrade on 11/09/2017.
+//  Copyright © 2017 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class AccountsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
